b8a15ff6adff42770616220777a4ac821ffeb1d0
Echo <646226046@qq.com>
2020-02-24 22:13:43 +0800

    storage封装以及接口错误拦截

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "camelcase": ["error", {"allow": ["aa_bb"]}],
+    "no-param-reassign": [2, { props: false }]
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import store from './store'
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  // eslint-disable-next-line prefer-const
+  let res = response.data
+  // eslint-disable-next-line eqeqeq
+  if (res.status == 0) {
+    return res.data
+  // eslint-disable-next-line eqeqeq
+  } else if (res.status == 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
 
+Vue.ues(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
