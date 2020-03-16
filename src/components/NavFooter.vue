2c8bb604510bf8f2e084c853d18b2c717b26a0c7
Echo <646226046@qq.com>
2020-03-16 23:39:40 +0800

    fix

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -5,10 +5,10 @@
           <p>小米商城</p>
         </div>
         <div class="footer-link">
-          <a href="">河畔一角主页</a>
-          <a href="">Vue全栈课程</a>
-          <a href="">React全家桶课程</a>
-          <a href="">微信支付专项课程(H5+小程序+Node+Mongo)</a>
+          <a href="https://www.baidu.com" target="_blank">河畔一角主页</a>
+          <a href="www.baidu.com">Vue全栈课程</a>
+          <a href="www.baidu.com">React全家桶课程</a>
+          <a href="www.baidu.com">微信支付专项课程(H5+小程序+Node+Mongo)</a>
         </div>
         <div class="copyright">© <span class="domain">mi.com</span> 京ICP证110507号 京ICP备10046444号 京公网安备11010802020134号 京网文[2020]0276-042号</div>
     </div>
