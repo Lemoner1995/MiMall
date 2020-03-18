df504226cec8e2824a068063c0eb0a3221efc38f
Echo <646226046@qq.com>
2020-03-18 23:01:17 +0800

    index广告图开发

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -56,8 +56,16 @@
             <div class="swiper-button-next" slot="button-next"></div>
           </swiper>
         </div>
-        <div class="ads-box"></div>
-        <div class="banner"></div>
+        <div class="ads-box">
+          <a :href="'/#/product/'+item.id" v-for="(item, index) in adsList" :key="index">
+            <img :src="item.img" alt="">
+          </a>
+        </div>
+        <div class="banner">
+          <a href="/#/product/30">
+            <img src="/imgs/banner-1.png" alt="">
+          </a>
+        </div>
         <div class="product-box"></div>
       </div>
       <service-bar></service-bar>
@@ -112,6 +120,12 @@ export default {
           { id: 33, img: '/imgs/item-box-4.jpg', name: '移动4G专区' }
         ],
         [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
+      ],
+      adsList: [
+        { id: 33, img: '/imgs/ads/ads-1.png' },
+        { id: 48, img: '/imgs/ads/ads-2.jpg' },
+        { id: 45, img: '/imgs/ads/ads-3.png' },
+        { id: 47, img: '/imgs/ads/ads-4.jpg' }
       ]
     }
   }
@@ -205,6 +219,18 @@ export default {
           height: 100%;
         }
       }
+    }
+    .ads-box{
+      margin-top: 14px;
+      margin-bottom: 31px;
+      @include flex();
+      a{
+        width: 296px;
+        height: 167px;
+      }
+    }
+    .banner{
+      margin-bottom: 50px;
     }
   }
 </style>
