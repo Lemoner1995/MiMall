49cc4c6582f6e33ba2dcf61f3440465a7e27013e
Echo <646226046@qq.com>
2020-03-25 22:10:37 +0800

    商品详情页导航栏开发，吸顶功能实现

---
+++ src/components/ProductParam.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="nav-bar" :class="{'is_fixed':isFixed}">
+        <div class="container">
+            <div class="pro-title">
+                小米10
+            </div>
+            <div class="pro-param">
+                <a href="javascript:;">概述</a><span>|</span>
+                <a href="javascript:;">参数</a><span>|</span>
+                <a href="javascript:;">用户评价</a>
+                <slot name="buy"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'productparam',
+  data () {
+    return {
+      isFixed: false
+    }
+  },
+  methods: {
+    initHeight () {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      // eslint-disable-next-line no-unneeded-ternary
+      this.isFixed = scrollTop > 152 ? true : false
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.initHeight)
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.initHeight, false)
+  }
+}
+</script>
+<style lang="scss">
+@import './../assets/scss/base.scss';
+@import './../assets/scss/reset.scss';
+@import './../assets/scss/config.scss';
+@import './../assets/scss/mixin.scss';
+@import './../assets/scss/modal.scss';
+@import './../assets/scss/button.scss';
+    .nav-bar{
+        height: 70px;
+        line-height: 70px;
+        border-top: 1px solid $colorH;
+        background-color: $colorG;
+        &.is_fixed{
+            width: 100%;
+            position: fixed;
+            top: 0;
+            box-shadow: 0 5px 5px $colorE;
+        }
+        .container{
+            @include flex();
+            .pro-title{
+                font-size: $fontH;
+                color: $colorB;
+                font-weight: bold;
+            }
+            .pro-param{
+                span{
+                    margin:0 10px ;
+                }
+                a{
+                   color: $colorC;
+                   &:last-child{
+                       margin-right: 10px;
+                   }
+                }
+                font-size: $fontJ;
+
+            }
+        }
+    }
+</style>
--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,12 +1,44 @@
 <template>
-    <div>
-      product
+    <div class="product">
+      <product-param>
+        <template v-slot:buy>
+          <button class="btn">立即购买</button>
+        </template>
+      </product-param>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
+      <div class="h">123</div>
     </div>
 </template>
 
 <script>
-
+import ProductParam from './../components/ProductParam'
 export default {
-  name: 'product'
+  name: 'product',
+  components: {
+    ProductParam
+  }
 }
 </script>
+<style lang="scss">
+  .product{
+    button{
+      margin-left: 10px;
+    }
+    .h{
+      background-color: green;
+      height: 150px;
+    }
+  }
+</style>
