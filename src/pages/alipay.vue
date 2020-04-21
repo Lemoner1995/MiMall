48e35e43479e54f6a8b9328baca80169b70545e6
Echo <646226046@qq.com>
2020-04-21 23:05:10 +0800

    支付宝/微信支付功能，订单页面以及订单支付页面开发

--- src/pages/alipay.vue
+++ src/pages/alipay.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
-      alipay
+      <div class="ali-pay">
+        <loading v-if="loading"></loading>
+        <div class="form" v-html="content"></div>
+      </div>
     </div>
 </template>
 
 <script>
-
+import Loading from '../components/Loading'
 export default {
-  name: 'alipay'
+  name: 'alipay',
+  data () {
+    return {
+      orderId: this.$route.query.orderId,
+      content: '',
+      loading: true
+    }
+  },
+  components: { Loading },
+  methods: {
+    paySubmit () {
+      this.axios.post('/pay', { orderId: this.orderId, orderName: 'Vue高仿小米商城', amount: '0.01', payType: 1 }).then((res) => {
+        this.content = res.content
+        // eslint-disable-next-line func-call-spacing
+        setTimeout (() => {
+          document.forms[0].submit()
+        }, 100)
+      })
+    }
+  },
+  mounted () {
+    this.paySubmit()
+  }
 }
 </script>
--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-      <order-header></order-header>
-      <h1>order</h1>
+      <order-header :title="title">
+        <template v-slot:tip>
+          <span>{{tip}}</span>
+        </template>
+      </order-header>
       <router-view></router-view>
+      <service-bar></service-bar>
       <order-footer></order-footer>
     </div>
 </template>
@@ -10,8 +14,32 @@
 <script>
 import OrderHeader from './../components/OrderHeader'
 import OrderFooter from './../components/OrderFooter'
+import ServiceBar from './../components/ServiceBar'
 export default {
   name: 'order',
-  components: { OrderHeader, OrderFooter }
+  components: { OrderHeader, OrderFooter, ServiceBar },
+  data () {
+    return {
+      title: '',
+      tip: ''
+    }
+  },
+  mounted () {
+    // eslint-disable-next-line prefer-const
+    let path = this.$route.path
+    if (path === '/order/confirm') {
+      this.title = '订单确认'
+      this.tip = '请认真填写收获地址'
+    } else if (path === '/order/list') {
+      this.title = '订单列表'
+      this.tip = '请谨防钓鱼链接呼噢诈骗电话，点击了解更多>'
+    } else if (path === '/order/pay') {
+      this.title = '订单支付'
+      this.tip = '请谨防钓鱼链接呼噢诈骗电话，点击了解更多'
+    } else if (path === '/order/alipay') {
+      this.title = '订单支付'
+      this.tip = '请谨防钓鱼链接呼噢诈骗电话，点击了解更多'
+    }
+  }
 }
 </script>
